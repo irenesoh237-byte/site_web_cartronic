465dc81b417439d8cc4e133455a2cb24672549ad
irenesoh237-byte <irenesoh237@gmail.com>
2026-07-22 12:56:03 +0100

    FEAT: resolve conflicts

--- src/app/pages/intervention/intervention.ts
+++ src/app/pages/intervention/intervention.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { StoreBadges } from '../../shared/store-badges/store-badges';
 import { TechnicianScene } from "../../shared/illustrations/technician-scene/technician-scene";
-import { TechnicianScene } from '../../shared/illustrations/technician-scene/technician-scene';
 
 @Component({
-  imports: [RouterLink, StoreBadges, TechnicianScene]
   imports: [RouterLink, StoreBadges, TechnicianScene],
   templateUrl: './intervention.html',
   styleUrl: './intervention.scss',
