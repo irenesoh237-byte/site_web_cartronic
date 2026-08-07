21d909bd3adb3628ae5d1760c5fa506ad4194f01
irenesoh237-byte <irenesoh237@gmail.com>
2026-08-07 14:12:41 +0100

    FEAT: ajout d'un nouveau membre

---
+++ src/app/shared/whatsapp-button/whatsapp-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsapp-button',
+  imports: [],
+  templateUrl: './whatsapp-button.html',
+  styleUrl: './whatsapp-button.scss',
+})
+export class WhatsappButton {}
